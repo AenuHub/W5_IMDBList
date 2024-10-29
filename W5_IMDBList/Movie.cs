87767ef9f79166abcadfe3047ea22c5f131358f3
AenuHub <jesuskane92@gmail.com>
2024-10-29 13:20:28 +0300

    add task implementation

--- W5_IMDBList/Movie.cs
+++ W5_IMDBList/Movie.cs
@@ -2,5 +2,45 @@
 
 public class Movie
 {
+    public string Title { get; set; }
+    public double Rating { get; set; }
     
+    public Movie(string title, double rating)
+    {
+        Title = title;
+        Rating = rating;
+    }
+    
+    // displaying all movies
+    public static void DisplayAllMovies(List<Movie> movieList)
+    {
+        foreach (var movie in movieList)
+        {
+            Console.WriteLine($"Title: {movie.Title} | Rating: {movie.Rating}");
+        }
+    }
+    
+    // displaying movies rated between 4 and 9
+    public static void DisplayGoodMovies(List<Movie> movieList)
+    {
+        foreach (var movie in movieList)
+        {
+            if (movie.Rating >= 4 && movie.Rating <= 9)
+            {
+                Console.WriteLine($"Title: {movie.Title} | Rating: {movie.Rating}");
+            }
+        }
+    }
+    
+    // displaying movies with title starting with "A"
+    public static void DisplayMoviesStartingWithA(List<Movie> movieList)
+    {
+        foreach (var movie in movieList)
+        {
+            if (movie.Title.StartsWith("A") || movie.Title.StartsWith("a"))
+            {
+                Console.WriteLine($"Title: {movie.Title} | Rating: {movie.Rating}");
+            }
+        }
+    }
 }
--- W5_IMDBList/Program.cs
+++ W5_IMDBList/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using W5_IMDBList;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // simple bool check for loop
+        bool ShouldContinue = true;
+        
+        List<Movie> movieList = new List<Movie>();
+        do
+        {
+            Console.Write("Enter movie title: ");
+            string title = Console.ReadLine();
+            Console.Write("Enter rating: ");
+            double rating = double.Parse(Console.ReadLine());
+            movieList.Add(new Movie(title, rating));
+
+            Console.WriteLine("------------------------------");
+            Console.Write("Press 'N' to quit or any other key to enter another movie: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("------------------------------");
+            if (input == "n" || input == "N")
+            {
+                ShouldContinue = false;
+            }
+        } while (ShouldContinue);
+        
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("All movies: ");
+        Movie.DisplayAllMovies(movieList);
+        
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Movies rated between 4 and 9: ");
+        Movie.DisplayGoodMovies(movieList);
+
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Movies with title starting with 'A': ");
+        Movie.DisplayMoviesStartingWithA(movieList);
+        
+        Console.ReadKey();
+    }
+}
